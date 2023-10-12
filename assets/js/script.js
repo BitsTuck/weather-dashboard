90345620de98b06619bee403de24e88584eaed19
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-11 22:39:34 -0400

    functional site, still needs local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var searchFormEl = $('.search-form');
 var todayWeatherEl = $('.today')
 var oneDayEl = $('.day')
+var searchCol = $('.history')
 var weatherHistory = localStorage.getItem(weatherHistory)
-console.log(oneDayEl)
+
+console.log(weatherHistory)
 
 
 function getApi(city) {
@@ -23,9 +25,11 @@ function getApi(city) {
         <p class="temp">Temperature: ${data.list[i*8].main.temp}&deg F</p>
         <p class="wind">Wind Speed: ${data.list[i*8].wind.speed} MPH</p>
         <p class="humidity">Humidity: ${data.list[i*8].main.humidity}%</p>`
+        // <span>${data.list[i*8].weather.icon}</span>
+
       } 
-      // weatherDisplay(data)
     })
+
     
    var searchCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1961d955221a31999d250c53fe92af36&units=imperial`
 
@@ -36,8 +40,19 @@ function getApi(city) {
     .then(function (data) {
       console.log(data)
       console.log(todayWeatherEl)
-      todayWeatherEl.innerHTML= `<h2>Today's Weather</h2>`
+      let todayHeader= document.createElement('h1');
+      todayHeader.textContent = `${data.name} Weather Today`
+      todayWeatherEl.append(todayHeader)
+      let todayInfo= document.createElement('p');
+      todayInfo.textContent = `Temperature: ${data.main.temp} Wind Speed: ${data.wind.speed} Humidity: ${data.main.humidity}`
+      todayWeatherEl.append(todayInfo)
+      // let todayIcon= document.createElement('span');
+      // todayIcon.setAttribute(`${data.weather[i].icon}`)
+      // todayWeatherEl.append(todayIcon)
+
   })
+
+
 
 
 }
@@ -55,104 +70,41 @@ function handleSearchFormSubmit(event) {
 
   getApi(searchInputVal)
 
-} 
+  // var cities = JSON.parse(localStorage.getItem('city'))
+  // console.log(cities)
+  // if (!cities) {
+  //   cities = []
+  // };
+
+  // cities.push(searchInputVal)
+
+  // localStorage.setItem('city', JSON.stringify(city))
+  // console.log(localStorage)
+
+}
 
 searchFormEl.on('submit', handleSearchFormSubmit);
 
+// var brewery = JSON.parse(localStorage.getItem('Brewery'));
 
-function weatherDisplay (data) {
-  console.log(data)
-  let fiveDayWeather = document.querySelector('.this-week')
-  fiveDayWeather.innerHTML = data.list;
-}
+// console.log(brewery);
 
+// for (var i = 0; i < brewery.length; i++) {
+//     var breweryName = brewery[i];
+//     var li = $('<li></li>');
+//     li.text(breweryName);
 
+//     $('.brewList').append(li);
+// };
 
-
-
-
-
-
-// function printResults(resultObj) {
-//   console.log(resultObj);
-
-//   // set up `<div>` to hold result content
-//   var resultCard = document.createElement('div');
-//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-//   var resultBody = document.createElement('div');
-//   resultBody.classList.add('card-body');
-//   resultCard.append(resultBody);
-
-//   var titleEl = document.createElement('h3');
-//   titleEl.textContent = resultObj.title;
-
-//   var bodyContentEl = document.createElement('p');
-//   bodyContentEl.innerHTML =
-//     '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-//   if (resultObj.subject) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> No subject for this entry.';
-//   }
-
-//   if (resultObj.description) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong> ' + resultObj.description[0];
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong>  No description for this entry.';
-//   }
-
-//   var linkButtonEl = document.createElement('a');
-//   linkButtonEl.textContent = 'Read More';
-//   linkButtonEl.setAttribute('href', resultObj.url);
-//   linkButtonEl.classList.add('btn', 'btn-dark');
-
-//   resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-//   resultContentEl.append(resultCard);
-// }
-
-// function searchApi(query, format) {
-//   var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
-
-//   if (format) {
-//     locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
-//   }
-
-//   locQueryUrl = locQueryUrl + '&q=' + query;
-//   console.log(locQueryUrl);
-
-//   fetch(locQueryUrl)
-//     .then(function (response) {
-
-//       if (!response.ok) {
-//         throw response.json();
-//       }
-
-//       return response.json();
-//     })
-//     .then(function (locRes) {
-//       // write query to page so user knows what they are viewing
-//       resultTextEl.textContent = locRes.search.query;
-
-//       console.log(locRes);
-
-//       if (!locRes.results.length) {
-//         console.log('No results found!');
-//         resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-//       } else {
-//         resultContentEl.textContent = '';
-//         for (var i = 0; i < locRes.results.length; i++) {
-//           printResults(locRes.results[i]);
-//         }
-//       }
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     });
-// }
+// $('.brewStorage').on('submit', function(event) {
+//   event.preventDefault();
+//   console.log($('.brewery').val());
+//   var favInput = $('.brewery').val();
+//   var brewery = JSON.parse(localStorage.getItem('Brewery'));
+//   if (!brewery) {
+//       brewery = [];
+//   };
+//   brewery.push(favInput);
+//   localStorage.setItem("Brewery", JSON.stringify(brewery));
+// })
